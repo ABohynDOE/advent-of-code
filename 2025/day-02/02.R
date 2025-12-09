c493f0513afa573cc228f676cd6f32e692bfcb50
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2025-12-09 14:04:57 +0100

    update day3

--- 2025/day-02/02.R
+++ 2025/day-02/02.R
@@ -1,7 +1,27 @@
+library(stringr)
 
 # Read input data ----
-input <- "2025/day-02/02-input"
+input <- readLines("2025/day-02/02-input")
 
-# Part 1 ----
+input |>
+  str_split_1(",") |>
+  lapply(str_split, "-") |>
+  unlist(recursive = FALSE) |>
+  purrr::map(\(x) seq(x[[1]], x[[2]])) |>
+  unlist() |>
+  as.character() |>
+  str_subset("^(\\d+)(\\1)$") |>
+  as.numeric() |>
+  sum()
 
-# Part 2 ----
+
+input |>
+  str_split_1(",") |>
+  lapply(str_split, "-") |>
+  unlist(recursive = FALSE) |>
+  purrr::map(\(x) seq(x[[1]], x[[2]])) |>
+  unlist() |>
+  as.character() |>
+  str_subset("^(\\d+)(\\1)+$") |>
+  as.numeric() |>
+  sum()
--- 2025/day-03/03.R
+++ 2025/day-03/03.R
@@ -2,18 +2,19 @@
 input <- readLines("2025/day-03/03-input") |>
   str_split("")
 
-max_joltage <- function(i) {
-  x <- i[[1]]
+max_joltage <- function(x) {
   l <- length(x)
   m <- max(x[-l], na.rm = TRUE)
   first_max <- which(x == m)[1]
   x2 <- x[-seq(1, first_max)]
   second_max <- max(x2, na.rm = TRUE)
-  num <- str_c(x[first_max], second_max) |> as.numeric()
+  str_c(x[first_max], second_max) |>
+    as.numeric()
 }
 
 # Part 1 ----
 
-purrr::map(input, max_joltage, .progress = T)
+purrr::map_dbl(input, max_joltage, .progress = T) |>
+  sum()
 
 # Part 2 ----
